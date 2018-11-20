74405dc3ddf49664168abd29b4b6bedc900ee3fc
root <root@cr1tst>
2018-11-20 10:10:51 +0300

    nnn2

--- stat_tst/settings.py
+++ stat_tst/settings.py
@@ -142,4 +142,4 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-S#TATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
